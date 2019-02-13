ff02bc13ef9611e9eaee39adbd0d963184f1614b
Tomas KOVAC <office@ztk-comp.sk>
2019-02-13 21:42:50 +0100

    Create locations.php

---
+++ locations.php
@@ -0,0 +1,6 @@
+<?php
+  if (($entries['lat'] > '48.721') and ($entries['lat'] < '48.722') and ($entries['lon'] > '21.215') and ($entries['lon'] < '21.216')) { $bgmiesto='#ffffd0';$miesto='KE-Benzinka-KVP';}
+  if (($entries['lat'] > '48.706') and ($entries['lat'] < '48.707') and ($entries['lon'] > '21.266') and ($entries['lon'] < '21.267')) { $bgmiesto='#ffffc0';$miesto='KE-Benzinka-Jantarova';}
+  if (($entries['lat'] > '48.722') and ($entries['lat'] < '48.724') and ($entries['lon'] > '21.227') and ($entries['lon'] < '21.229')) { $bgmiesto='#ffffb0';$miesto='KE-Benzinka-Popradska';}
+  if (($entries['lat'] > '48.739') and ($entries['lat'] < '48.740') and ($entries['lon'] > '21.255') and ($entries['lon'] < '21.256')) { $bgmiesto='#ffffa0';$miesto='KE-Benzinka-Hlinkova';}
+?>
